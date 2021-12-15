9b4154b741edffd0ceb287f73056f1c7fa2036f9
WanlinS0705 <ssswl2001@163.com>
2021-12-15 23:16:10 +0800

    Initial commit

---
+++ src/TestPush.java
@@ -0,0 +1,15 @@
+public class TestPush {
+    public static void main(String[] args){
+
+        shenwanlin();
+        test();
+    }
+    public  static void shenwanlin(){
+        System.out.println("Name: WanlinShen, ID: 19406040112");
+    }
+    public static  void test(){
+        System.out.println("Name: test, ID: test");
+    }
+
+
+}
